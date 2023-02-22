1709b5558ad8348a608c5f907cac8d43be80656b
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-02-22 18:59:25 +0100

    side Redo

--- js/side-bar.js
+++ js/side-bar.js
@@ -5,14 +5,18 @@ window.addEventListener('load',function(){
     const hoverClass = document.querySelector(".hover-bar");
     const sections = document.querySelectorAll(".hover-mid");
     menuOption.forEach(function (item) {
-        item.addEventListener('click', function(){
-            if( this.dataset.hover !== undefined ){
+        item.addEventListener('click', function () {
+
+            if (this.dataset.hover !== undefined) {
+
                 menuOption.forEach(function (m) {
-                    m.classList.remove('chosen-section'); 
+                    m.classList.remove('chosen-section');
+                    console.log(m);
                 });
                 const chosenSection = this.dataset.hover;
                 sections.forEach(function (s) {
                     if(chosenSection === s.id){
+
                         let containsHover = document.getElementById(chosenSection).classList.contains('active-hover');
                         switch(containsHover){
                             case false:
@@ -29,7 +33,7 @@ window.addEventListener('load',function(){
                             break;
                         }
                     } else {
-                        document.getElementById(s.id).classList.remove('active-hover'); 
+                        document.getElementById(s.id).classList.remove('active-hover');
                     }
                 });
             }
@@ -38,25 +42,103 @@ window.addEventListener('load',function(){
     // Responsive
     const sideBarOptions = document.querySelector(".side-mid");
     menuBtn.addEventListener('click', function(){
-        let icon = this.querySelector('.fa-bars');
-        let displayCheck = window.getComputedStyle(sideBarOptions).display;
-        if(displayCheck == 'none'){
-            sideBarOptions.style.display = 'block';
-            icon = this.querySelector('.fa-bars');
-            icon.classList.add('fa-xmark');
-            icon.classList.remove('fa-bars');
-        } else {
+      let icon = document.querySelector('.fa-bars');
+      let displayCheck = window.getComputedStyle(sideBarOptions).display;
+      if(displayCheck == 'none'){
+        sideBarOptions.style.display = 'block';
+        icon = this.querySelector('.fa-bars');
+        icon.classList.add('fa-xmark');
+        icon.classList.remove('fa-bars');
+      } else {
+          console.log(displayCheck);
             sideBarOptions.style.display = 'none';
             icon = this.querySelector('.fa-xmark');
             icon.classList.add('fa-bars');
             icon.classList.remove('fa-xmark');
             menuOption.forEach(function (m) {
-                m.classList.remove('chosen-section'); 
+                m.classList.remove('chosen-section');
             });
             hoverClass.classList.remove('active-hover');
             sections.forEach(function(s){
-                document.getElementById(s.id).classList.remove('active-hover'); 
+                document.getElementById(s.id).classList.remove('active-hover');
             });
         }
     });
 });
+
+// window.addEventListener("load", onPageLoad);
+
+// function onPageLoad() {
+//   const refs = {
+//     menuElements: document.querySelectorAll(
+//       ".side-element"
+//     ),
+//     sideBar: document.querySelector(".side-bar"),
+//     hoverBar:
+//       document.querySelector(".hover-bar"),
+//     hoverBarItems:
+//       document.querySelectorAll(".hover-mid"),
+//   };
+ 
+//     refs.sideBar.addEventListener(
+//       "click",
+//       onSideBarClick
+//     );
+
+
+//   function onSideBarClick(evt) {
+//     refs.menuElements.forEach((menu) => {
+//       menu.classList.remove("chosen-section");
+//     });
+// console.log(evt.target);
+//     if (
+//       evt.target.closest(".side-element") !== null
+//     ) {
+//       const chosenMenu = evt.target.closest(
+//         ".side-element"
+//       );
+
+//       refs.hoverBarItems.forEach((item) => {
+//         if (
+//           item.id === chosenMenu.dataset.hover
+//         ) {
+//           if (
+//             !document
+//               .getElementById(
+//                 `${chosenMenu.dataset.hover}`
+//               )
+//               .classList.contains("active-hover")
+//           ) {
+            
+//             chosenMenu.classList.add(
+//               "chosen-section"
+//             );
+
+//             document
+//               .getElementById(
+//                 `${chosenMenu.dataset.hover}`
+//               )
+//               .classList.add("active-hover");
+//             refs.hoverBar.classList.add(
+//               "active-hover"
+//             );
+//            return
+//           }else {
+//               chosenMenu.classList.remove(
+//                 "chosen-section"
+//               );
+//               document
+//                 .getElementById(
+//                   `${chosenMenu.dataset.hover}`
+//                 )
+//                 .classList.remove("active-hover");
+//               refs.hoverBar.classList.remove(
+//                 "active-hover"
+//               );
+//            return
+//           }
+//         } else item.classList.remove("active-hover");
+//       });
+//     }
+//   }
+// }
